8a3a22f3f708a61538c479d7946c089ac48341da
HAMeed220 <singanamalahameed11@.com>
2024-01-24 15:05:06 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -5,6 +5,10 @@ from django.shortcuts import render
 from app.forms import *
 from django.http import HttpResponse
 from django.core.mail import send_mail
+from django.http import HttpResponse ,HttpResponseRedirect
+from django.contrib.auth import authenticate,login
+from django.urls import reverse
+
 
 def Registration(request):
     ufo=UserForm()
@@ -35,3 +39,24 @@ def Registration(request):
             return HttpResponse('Invalid')
     return render(request,'Registration.html',d)
 
+def home(request):
+    if request.session.get('username'):
+        username=request.session.get('username')
+        d={'username':username}
+        return render(request,'home.html',d)
+    return render(request,'home.html')
+
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST['un']
+        password=request.POST['pw']
+        AUO=authenticate(username=username,password=password)
+
+        if AUO and AUO.is_active:
+            login(request,AUO)
+            request.session['username']=username
+            return HttpResponseRedirect(reverse('home'))
+        else:
+            return HttpResponse('Invalid Credentials')
+
+    return render(request,'user_login.html')
